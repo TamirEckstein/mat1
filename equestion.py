0d22486424cac8de3b6bcfe11b7609af85eaa683
tamir eckstein <tamireckstein@gmail.com>
2021-03-24 20:37:34 +0200

    new one

--- equestion.py
+++ equestion.py
@@ -2,7 +2,6 @@ def XtimesY(x:float,y:float)->float:
     if x<=0:
         return 0
     lmala = Ln(x)*y
-    print(exponent(lmala))
     return (exponent(lmala))
     
    
@@ -27,22 +26,17 @@ def Ln(x:float)->float:
                
 def sqrt(x:float,y:float) -> float:
     if y<=0:
-        return print(0)
+        return 0
     ans=XtimesY(y, (1/x))      
     return ans    
 
 def calculate(x:float) -> float:
     if x<=0:
         return 0
-    ansCac:float= (exponent(x))
-    ansSq:float=(sqrt(x,x))
-    ans1:float=(XtimesY(x, -1.0))
-    ans2:float=(XtimesY(7.0, x))
+    ansCac= (exponent(x))
+    ansSq=(sqrt(x,x))
+    ans1=(XtimesY(x, -1.0))
+    ans2=(XtimesY(7.0, x))
     total=ans1*ans2*ansCac*ansSq
-    print(total)
-    
-    
-XtimesY(3,-1)
-sqrt(0.5, 3)
-calculate(3)
+    return total
     
